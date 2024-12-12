f93af1343ee6b971e8ef1b7d3a9d25518854e167
alozovaia <anyalozovaya2014@gmail.com>
2024-12-12 12:50:09 +0300

    fix 3

--- src/components/AboutMe/Description.vue
+++ src/components/AboutMe/Description.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="description">
+    1
     <div class="description_text">
       <p v-for="(text, index) in profile" :key="index">
         {{ index !== profile.length - 1 ? text : null }}

a0565ad4e268e065cb0cab6e89a488c079f79a58
alozovaia <anyalozovaya2014@gmail.com>
2024-12-12 12:38:56 +0300

    commit 4

--- src/components/AboutMe/AboutMe.vue
+++ src/components/AboutMe/AboutMe.vue
@@ -2,7 +2,6 @@
   <div class="about-me">
     <div class="about-me_wrapper">
       1
-      2
       3
       <my-info :person-name="FIO" :person-job="job" :person-CV="urlCV"></my-info>
       <my-description

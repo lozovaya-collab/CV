ea44f682b40e9797b7c66383b36ab6796e425505
alozovaia <anyalozovaya2014@gmail.com>
2024-12-12 15:15:37 +0300

    commit 6

--- src/components/AboutMe/Info.vue
+++ src/components/AboutMe/Info.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="info">
-    5
+    55
     <img src="../../assets/avatar.png" alt="Avatar" class="info_avatar" />
     <div class="info_wrapper">
       <p class="info_FIO">{{ personName }}</p>

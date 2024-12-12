8f0ee3a97ec4d87dd874ac87b221a058b88d7bb7
alozovaia <anyalozovaya2014@gmail.com>
2024-12-12 12:26:01 +0300

    [fix]

--- src/components/AboutMe/AboutMe.vue
+++ src/components/AboutMe/AboutMe.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="about-me">
     <div class="about-me_wrapper">
-      вавав
+      ва
       <my-info :person-name="FIO" :person-job="job" :person-CV="urlCV"></my-info>
       <my-description
         :href-hackaton="hrefHackaton"
